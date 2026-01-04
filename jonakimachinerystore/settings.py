cb7d890013663b72e88a1c226ea025dcef6c96c7
Mahi Uddin <mahi.ewu@gmail.com>
2026-01-04 21:44:34 +0100

    debug mode on

--- jonakimachinerystore/settings.py
+++ jonakimachinerystore/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['95.217.7.154', '127.0.0.1', 'localhost', 'jonaki.insafee.com','www.jonaki.insafee.com']
 
